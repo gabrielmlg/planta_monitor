e14dc830722daa02cd84709ad12c3a536d5a9aa2
gabrielmlg <gabrielopes@gmail.com>
2020-11-24 14:45:06 -0300

    bug fix

--- main.py
+++ main.py
@@ -24,47 +24,44 @@ def main():
     count = 1
 
     try: 
-        df = pd.read_csv('Documents/dev/planta_monitor/app/dataset/clotilde_v1.csv')
+        df = pd.read_csv('Users/gabriel/Documents/dev/planta_monitor/app/dataset/clotilde_v1.csv')
         print('Carregou ...')
     except:
         df = pd.DataFrame()
 
-    try:
-        while True:
-            for c in s.read():
-                seq.append(chr(c)) #convert from ANSII
-                joined_seq = ''.join(str(v) for v in seq) #Make a string from array
+    while True:
+        for c in s.read():
+            seq.append(chr(c)) #convert from ANSII
+            joined_seq = ''.join(str(v) for v in seq) #Make a string from array
 
-                if chr(c) == '\n':
-                    doc_clotilde = eval(joined_seq)
-                    # print(doc_clotilde['umidade_solo'])
-                    data.append(datetime.now())
-                    umidade.append(doc_clotilde['umidade_solo'])
-                    temperatura.append(doc_clotilde['umidade_solo'])
-
-
-                    df2 = pd.DataFrame({
-                        'data': data, 
-                        'umidade': umidade, 
-                        'temperatura': temperatura
-                    })
-
-                    #df = df.append(df2, ignore_index=True)
-                    #df['data'] = pd.to_datetime(df['data'])
-                    #df.sort_values('data', inplace=True)
-
-                    df2.to_csv('Documents/dev/planta_monitor/app/dataset/clotilde_v1.csv', index=False)
-
-                    print(df2.tail())
-                    #print("Line " + str(count) + ': ' + joined_seq)
-                    seq = []
-                    count += 1
-                    break
+            if chr(c) == '\n':
+                doc_clotilde = eval(joined_seq)
+                # print(doc_clotilde['umidade_solo'])
+                data.append(datetime.now())
+                umidade.append(doc_clotilde['umidade_solo'])
+                temperatura.append(doc_clotilde['umidade_solo'])
 
 
-        ser.close()
-    except: 
-        print('ERROR!!!!')
+                df2 = pd.DataFrame({
+                    'data': data, 
+                    'umidade': umidade, 
+                    'temperatura': temperatura
+                })
+
+                #df = df.append(df2, ignore_index=True)
+                #df['data'] = pd.to_datetime(df['data'])
+                #df.sort_values('data', inplace=True)
+
+                df2.to_csv('/Users/gabriel/Documents/dev/planta_monitor/app/dataset/clotilde_v1.csv', index=False)
+
+                print(df2.tail())
+                #print("Line " + str(count) + ': ' + joined_seq)
+                seq = []
+                count += 1
+                break
+
+
+    ser.close()
 
 
 main()
